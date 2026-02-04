2c4e577ceda2ab060b4a3b97374a0d522571c518
phamvuminhtoana4-lang <phamvuminhtoana4@gmail.com>
2026-02-04 21:08:03 +0700

    Update vfx.js

--- vfx.js
+++ vfx.js
@@ -1,1 +1,24 @@
+const VFX = {
+    items: [],
+    spawn(x, y, color, type = 'particle') {
+        for(let i=0; i<15; i++) {
+            this.items.push({
+                x, y, c: color, t: type,
+                vx: (Math.random()-0.5)*12,
+                vy: (Math.random()-0.5)*12,
+                l: 30
+            });
+        }
+    },
+    updateAndDraw(ctx) {
+        this.items = this.items.filter(p => {
+            ctx.globalAlpha = p.l / 30;
+            ctx.fillStyle = p.c;
+            ctx.beginPath(); ctx.arc(p.x, p.y, 3, 0, 7); ctx.fill();
+            p.x += p.vx; p.y += p.vy; p.l--;
+            return p.l > 0;
+        });
+        ctx.globalAlpha = 1;
+    }
+};
 
