e8946e8ea9466e5f7d73e5dd71b49170e1d9b58e
phamvuminhtoana4-lang <phamvuminhtoana4@gmail.com>
2026-02-04 21:02:15 +0700

    Update sound.js

--- sound.js
+++ sound.js
@@ -1,1 +1,13 @@
+const Sound = {
+    ctx: new (window.AudioContext || window.webkitAudioContext)(),
+    playAtk() {
+        const osc = this.ctx.createOscillator();
+        const gain = this.ctx.createGain();
+        osc.connect(gain); gain.connect(this.ctx.destination);
+        osc.type = 'square'; osc.frequency.value = 150;
+        gain.gain.setValueAtTime(0.1, this.ctx.currentTime);
+        gain.gain.exponentialRampToValueAtTime(0.01, this.ctx.currentTime + 0.1);
+        osc.start(); osc.stop(this.ctx.currentTime + 0.1);
+    }
+};
 
