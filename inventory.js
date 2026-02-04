17a583564c7d887c2b3bb11568835559543f9227
phamvuminhtoana4-lang <phamvuminhtoana4@gmail.com>
2026-02-04 20:49:09 +0700

    Update inventory.js

--- inventory.js
+++ inventory.js
@@ -1,1 +1,13 @@
+const Inventory = {
+    items: [],
+    add(itemName) {
+        this.items.push(itemName);
+        Logger.log(`Đã nhặt: ${itemName}`);
+    },
+    use(index) {
+        const item = this.items[index];
+        if(item === 'Đại Hoàn Đan') Game.p.hp = Game.p.maxHp;
+        this.items.splice(index, 1);
+    }
+};
 
