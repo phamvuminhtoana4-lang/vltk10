dc57d4fdeda2b9697e134a62cdd58ceebd5c0f26
phamvuminhtoana4-lang <phamvuminhtoana4@gmail.com>
2026-02-04 20:45:45 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,15 @@
+const CONFIG = {
+    worldSize: 10000,
+    sects: [
+        { n: 'Thiếu Lâm', c: '#ffcc00', s: 'Như Lai Thần Chưởng' },
+        { n: 'Võ Đang', c: '#00ffff', s: 'Thái Cực Kiếm' },
+        { n: 'Minh Giáo', c: '#ff3300', s: 'Càn Khôn Đại Na Di' }
+    ],
+    ranks: ["Phàm Nhân", "Luyện Khí", "Trúc Cơ", "Kim Đan", "Nguyên Anh", "Đại Đế"],
+    weapons: [
+        { n: 'Trúc Kiếm', p: 1000, d: 30 },
+        { n: 'Ỷ Thiên Kiếm', p: 10000, d: 150 },
+        { n: 'Đồ Long Đao', p: 50000, d: 500 }
+    ]
+};
 
