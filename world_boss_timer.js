38af8cd83fe5264a8e79dad5629022696a03b953
phamvuminhtoana4-lang <phamvuminhtoana4@gmail.com>
2026-02-04 21:12:15 +0700

    Update world_boss_timer.js

--- world_boss_timer.js
+++ world_boss_timer.js
@@ -1,1 +1,21 @@
+const WorldBossTimer = {
+    timeLeft: 300, // 5 phút
+    active: false,
+    update() {
+        if (this.timeLeft > 0) {
+            this.timeLeft--;
+        } else if (!this.active) {
+            this.active = true;
+            BossSystem.spawn(Game.p.x, Game.p.y);
+            Logger.log("🔥 MA VƯƠNG ĐÃ XUẤT THẾ! TIÊU DIỆT NGAY!");
+        }
+    },
+    render() {
+        const m = Math.floor(this.timeLeft / 60);
+        const s = this.timeLeft % 60;
+        return `<div class="panel" style="top:10px; left:50%; transform:translateX(-50%); color:red; border-color:red">
+            BOSS XUẤT HIỆN SAU: ${m}:${s < 10 ? '0' + s : s}
+        </div>`;
+    }
+};
 
