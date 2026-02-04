eac3aa6ec555d900c6b8886ff8bad9868def6aff
phamvuminhtoana4-lang <phamvuminhtoana4@gmail.com>
2026-02-04 21:10:13 +0700

    Update wings_evolution.js

--- wings_evolution.js
+++ wings_evolution.js
@@ -1,1 +1,25 @@
+const Wings = {
+    tiers: [
+        { name: 'Yên Vũ', color: '#fff', size: 30, glow: 10 },
+        { name: 'Hàn Băng', color: '#00f', size: 50, glow: 20 },
+        { name: 'Phượng Hoàng', color: '#f00', size: 70, glow: 35 },
+        { name: 'Thái Cổ Thần Dực', color: '#gold', size: 100, glow: 50 }
+    ],
+    draw(ctx, x, y, tierIndex) {
+        const t = this.tiers[tierIndex];
+        ctx.save();
+        ctx.strokeStyle = t.color;
+        ctx.shadowBlur = t.glow;
+        ctx.shadowColor = t.color;
+        ctx.lineWidth = 3;
+        // Vẽ 2 cánh đối xứng
+        for (let side of [-1, 1]) {
+            ctx.beginPath();
+            ctx.moveTo(x, y - 40);
+            ctx.bezierCurveTo(x + 50 * side, y - 100, x + 100 * side, y, x, y + 20);
+            ctx.stroke();
+        }
+        ctx.restore();
+    }
+};
 
