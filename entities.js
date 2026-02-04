86e9b9be915723a69d56d5737efec9c0eda21f62
phamvuminhtoana4-lang <phamvuminhtoana4@gmail.com>
2026-02-04 20:48:12 +0700

    Update entities.js

--- entities.js
+++ entities.js
@@ -1,1 +1,15 @@
+class Stickman {
+    constructor(x, y, color, isPlayer = false) {
+        this.x = x; this.y = y; this.color = color;
+        this.hp = 200; this.maxHp = 200; this.isPlayer = isPlayer;
+    }
+    draw(ctx, lean = 0, scale = 1) {
+        ctx.save(); ctx.translate(this.x, this.y); ctx.rotate(lean);
+        ctx.strokeStyle = this.color; ctx.lineWidth = 4 * scale;
+        // Body logic
+        ctx.beginPath(); ctx.arc(0, -45*scale, 12*scale, 0, 7); ctx.stroke(); // Đầu
+        ctx.beginPath(); ctx.moveTo(0, -35*scale); ctx.lineTo(0, 5*scale); ctx.stroke(); // Thân
+        ctx.restore();
+    }
+}
 
