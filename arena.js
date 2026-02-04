1efe8f302a96a1190ba0e62085b0bc9a80868168
phamvuminhtoana4-lang <phamvuminhtoana4@gmail.com>
2026-02-04 20:39:43 +0700

    Update arena.js

--- arena.js
+++ arena.js
@@ -1,1 +1,16 @@
 
+const Mounts = {
+    types: {
+        'Ngựa': { speed: 1.5, color: '#8b4513' },
+        'Hỏa Kỳ Lân': { speed: 2.5, color: '#ff4400' }
+    },
+    draw(ctx, x, y, type) {
+        const m = this.types[type];
+        ctx.strokeStyle = m.color;
+        ctx.shadowBlur = 15; ctx.shadowColor = m.color;
+        ctx.beginPath();
+        ctx.ellipse(x, y + 20, 40, 15, 0, 0, Math.PI * 2);
+        ctx.stroke();
+        ctx.shadowBlur = 0;
+    }
+};
