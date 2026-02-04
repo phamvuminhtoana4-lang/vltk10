4198c4ae8c9ac6c67fe6d4406352e30ce1379648
phamvuminhtoana4-lang <phamvuminhtoana4@gmail.com>
2026-02-04 20:42:19 +0700

    Update cheat_codes.js

--- cheat_codes.js
+++ cheat_codes.js
@@ -1,1 +1,21 @@
+const Cheats = {
+    init() {
+        window.addEventListener('keydown', (e) => {
+            if (e.key === 'Insert') {
+                const code = prompt("Nhập Lệnh Bài:");
+                this.apply(code);
+            }
+        });
+    },
+    apply(code) {
+        if (code === "VODICH") {
+            Game.p.hp = 999999; Game.p.dmg = 9999;
+            Logger.log("Đã kích hoạt chế độ Vô Địch!");
+        }
+        if (code === "LAMGIAU") {
+            Game.p.gold += 100000;
+            Logger.log("Nhận 100.000 Vàng!");
+        }
+    }
+};
 
