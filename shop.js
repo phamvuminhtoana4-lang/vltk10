e1a2033c64d4d2953a942518b55f226d1f88b8af
phamvuminhtoana4-lang <phamvuminhtoana4@gmail.com>
2026-02-04 21:00:43 +0700

    Update shop.js

--- shop.js
+++ shop.js
@@ -1,1 +1,13 @@
+const Shop = {
+    open() {
+        let html = '<div id="shop-p" class="panel" style="top:50%; left:50%; transform:translate(-50%,-50%); width:300px">';
+        CONFIG.weapons.forEach((w, i) => {
+            html += `<div style="padding:10px; border-bottom:1px solid #444">
+                ${w.n} (+${w.d}) - <button onclick="Game.buy(${i})">${w.p}V</button>
+            </div>`;
+        });
+        html += '<button onclick="this.parentElement.remove()">ĐÓNG</button></div>';
+        document.body.insertAdjacentHTML('beforeend', html);
+    }
+};
 
